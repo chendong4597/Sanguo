faffc94aa692129783774506433db14602f57819
chendong4597 <chendong4597@163.com>
2025-08-19 16:22:17 +0800

    begin

---
+++ Client/Sanguo/Source/Sanguo/Sanguo.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sanguo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sanguo, "Sanguo" );
+
+DEFINE_LOG_CATEGORY(LogSanguo)
+ 
---
+++ Client/Sanguo/Source/Sanguo/SanguoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SanguoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASanguoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASanguoGameMode();
+};
+
+
+
